ea6ab0cd9e77091517409d33c0577156940ebf7e
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2024-09-26 12:01:45 +0600

    Refactor image imports in about page and adjust margins in header and widget list icon

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import  "bootstrap/dist/css/bootstrap.min.css"
-import { iAccordionImage, iAuthor, iCountryImage, iCountryImage2, iCountryImage3, iCountryImage4, iCountryImage5, iCountryImage6, iCountryImage7, iDestinationImage, iDestinationImage2, iDestinationImage3, iDestinationImage4, iFlag, iFlag2, iFlag3, iFlag4, iFlag5, iFlag6, iFlag7, iFlag8, iGlobalIcon, iGoogleIcon, iLogo, iOfferBannerCard, iOfferBannerCard2, iPaymentIcon, iPaymentIcon2, iPaymentIcon3, iPaymentIcon4, iPaymentIcon5, iPaymentIcon6, iPaymentIcon7, iPaymentIcon8, iPaymentIcon9, iVisaSection } from "@/util/imageImports";
+import { iAccordionImage,iCountryImage, iCountryImage2, iCountryImage3, iCountryImage4, iCountryImage5, iCountryImage6, iCountryImage7, iDestinationImage, iDestinationImage2, iDestinationImage3, iDestinationImage4, iFlag, iFlag2, iFlag3, iFlag4, iFlag5, iFlag6, iFlag7, iFlag8, iLogo, iVisaSection } from "@/util/imageImports";
 import "../app/css/style.css"
 import OfferBannerSlider from "./components/OfferBannerSlider";
 import ClientSpeak from "./components/ClientSpeak";
 import ProvideVisa from "./components/Providevisa";
+import Link from "next/link";
 export default function Home() {
 
     const backgroundImageStyle = {
@@ -30,16 +31,16 @@ export default function Home() {
                     <a style={{textDecoration:"none"}} href="#">Global Visa</a>
                     <i className="bi bi-plus dropdown-icon"></i>
                     <ul className="sub-menu">
-                        <li><a style={{textDecoration:"none"}} href="#">Global Visa1</a></li>
-                        <li><a style={{textDecoration:"none"}} href="#">Global Visa2</a></li>
-                        <li><a style={{textDecoration:"none"}} href="#">Global Visa3</a></li>
+                        <li><Link  href="/">Global Visa1</Link></li>
+                        <li><Link  href="/">Global Visa2</Link></li>
+                        <li><Link  href="/">Global Visa3</Link></li>
                     </ul>
                 </li>
-                <li><a style={{textDecoration:"none"}} href="#">UAE Attraction</a></li>
-                <li><a style={{textDecoration:"none"}} href="#">holiday package</a></li>
-                <li><a style={{textDecoration:"none"}} href="#">Travel insurance</a></li>
+                <li><Link  href="/">UAE Attraction</Link></li>
+                <li><Link  href="/">holiday package</Link></li>
+                <li><Link href="/">Travel insurance</Link></li>
                 <li>
-                    <a style={{textDecoration:"none"}} href="about-us.html" className="drop-down"> About Us</a>
+                    <Link href="/about" className="drop-down"> About Us</Link>
                 </li>
             </ul>
             <div className="d-xl-none d-block">
@@ -66,9 +67,7 @@ export default function Home() {
             <div className="swiper-wrapper">
                 <div className="swiper-slide">
                     <div className="home1-banner-wrapper"
-                    style={backgroundImageStyle}
-                        // style="background-image: linear-gradient(180deg, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(assets/image/global-sky-banner.jpg);"
-                        >
+                    style={backgroundImageStyle}>
                         <div className="container">
                             <div className="row">
                                 <div className="col-lg-12">
@@ -95,10 +94,7 @@ export default function Home() {
                     </div>
                 </div>
                 <div className="swiper-slide">
-                    <div className="home1-banner-wrapper"
-                    style={backgroundImageStyle}
-                        // style="background-image: linear-gradient(180deg, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(assets/image/global-sky-banner.jpg);"
-                        >
+                    <div className="home1-banner-wrapper" style={backgroundImageStyle}>
                         <div className="container">
                             <div className="row">
                                 <div className="col-lg-12">
